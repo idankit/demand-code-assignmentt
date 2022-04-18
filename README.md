5ef5d77988d799844e0248095345080339d32571
idankit <idan.kitenberg@is.com>
2022-04-19 02:13:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-vbuqvz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-vbuqvz)
---
+++ index.js
@@ -0,0 +1,10 @@
+async function getCampaigns(sortBy, sortDirection) {}
+
+async function getCampaign(id) {}
+
+async function saveCampaign(name, startDate, bid) {}
+
+// getCampaigns()
+// getCampaigns('name', 'desc')
+// getCampaign(2);
+// saveCampaign('Test campaign new', '2023-01-01');
